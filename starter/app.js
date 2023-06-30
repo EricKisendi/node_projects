36d84b88cdcb01d951c6e9a0832bedfebab7e335
eric kisendi <erickisendi18@gmail.com>
2023-06-30 08:26:24 +0300

    first commit

---
+++ starter/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks.js')
+const connetDB = require('./db/connect.js')
+require('dotenv').config()
+//middleware
+
+app.use(express.json())
+
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send('Welcome')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 3000
+
+const start = async () => {
+    try {
+        await connetDB (process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()

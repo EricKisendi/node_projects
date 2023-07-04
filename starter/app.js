f8a885f55be263024d8e134959dd3c850daeb2c0
eric kisendi <erickisendi18@gmail.com>
2023-07-04 14:45:00 +0300

    error handler

--- starter/app.js
+++ starter/app.js
@@ -3,20 +3,20 @@ const app = express();
 const tasks = require('./routes/tasks.js')
 const connetDB = require('./db/connect.js')
 require('dotenv').config()
-const notfound = require('./middleware/not-found.js')
+const errorHandlerMiddleware = require('./middleware/error.js')
 //middleware
 //app.use(express.static('./public'))
 app.use(express.json())
 
 
 //routes
-app.get('/hello', (req, res) => {
-    res.send('Welcome')
-})
+// app.get('/hello', (req, res) => {
+//     res.send('Welcome')
+// })
 
 app.use('/api/v1/tasks', tasks)
 
-app.use(notfound)
+app.use(errorHandlerMiddleware)
 
 const port = 3000
 
--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -13,8 +13,8 @@ const createTask = asyncWrapper(async (req, res) => {
         res.status(201).json({task})
 })
 const getTask = asyncWrapper(async (req, res) => {
-        const {id:taskID} = req.params
-        const task = await Task.findOne({_id:taskID})
+    const {id:taskID} = req.params
+    const task = await Task.findOne({_id:taskID})
     if(!task){
         return res.status(404).json({ msg:`No task with id : ${taskID}` })
     }
@@ -22,8 +22,8 @@ const getTask = asyncWrapper(async (req, res) => {
 })
 
 const deleteTask = asyncWrapper(async (req, res) => {
-        const {id:taskID} = req.params
-        const task = await Task.findOneAndDelete({_id:taskID})
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndDelete({_id:taskID})
     if(!task){
         return res.status(404).json({ msg:`No task with id : ${taskID}` })
     }
